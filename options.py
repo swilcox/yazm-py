09b28012c251e4f85a775800ccba4ffee89f2e8d
swilcox <steven@wilcoxzone.com>
2019-05-04 12:02:47 -0500

    stub options

---
+++ options.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Options:
+    save_dir: str
+    save_name: str
+    log_instructions: bool
+    rand_seed: list
+
+    @classmethod
+    def default(cls):
+        return cls(
+            save_dir='',
+            save_name='',
+            log_instructions=False,
+            rand_seed=bytearray([90, 111, 114, 107])
+        )
