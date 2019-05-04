24461a4bb0e91468ac7f0535fbd0b47b2bff2dff
swilcox <steven@wilcoxzone.com>
2019-05-04 12:02:00 -0500

    stub out test files.

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+from utils import from_u16_to_i16
+
+
+def test_from_u16_to_i16():
+    assert 1 == from_u16_to_i16(1)
+    assert 0 == from_u16_to_i16(0)
+    assert 0 == from_u16_to_i16(32768)
+    assert 32767 == from_u16_to_i16(32767)
+    assert -1232 == from_u16_to_i16(34000)
+    assert -32767 == from_u16_to_i16(65535)
